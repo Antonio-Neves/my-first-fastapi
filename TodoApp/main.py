71cf0023844b84a6a114d668505f75f96284122a
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-01-23 09:51:50 +0000

    Created first endpoint

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
-from database import engine
-import models
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import Base
+from models import Todos
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
+
