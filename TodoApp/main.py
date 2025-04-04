fc61e4c624540ce084a80183cdb7311752bc06f0
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-04-04 11:30:05 +0100

    2025 03 02 1100

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
+from pydantic import BaseModel, Field
 import models
 from models import Todos
 
@@ -9,6 +11,14 @@ from models import Todos
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
 
 
 def get_db():
@@ -19,8 +29,49 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependecy = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependecy):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependecy, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise HTTPException(status_code=404, detail='Todo not found')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependecy, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependecy,
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)):
+    
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+
